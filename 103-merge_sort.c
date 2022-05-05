90aa25bad5e620f860c1aa31df4210a5d31e24ea
3599josephw <3599@holbertonschool.com>
2022-05-05 11:50:40 -0500

    betty

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -12,10 +12,9 @@
  */
 void merge(int *arr, int start, int mid, int end, int *temp)
 {
-	/* crawlers for both intervals and for temp */
 	int i = start, j = mid + 1, k = 0;
 
-	/* traverse both arrays and in each iteration add smaller of both elements in temp */
+	/* traverse arrays, each iteration add smaller of both elements in temp */
 	while (i <= mid && j <= end)
 	{
 		if (arr[i] <= arr[j])
@@ -30,16 +29,13 @@ void merge(int *arr, int start, int mid, int end, int *temp)
 			j += 1;
 		}
 	}
-
 	/* add elements left in the first interval */
-	
 	while (i <= mid)
 	{
 		temp[k] = arr[i];
 		k += 1;
 		i += 1;
 	}
-
 	/* add elements left in the second interval */
 	while (j <= end)
 	{
@@ -47,7 +43,6 @@ void merge(int *arr, int start, int mid, int end, int *temp)
 		k += 1;
 		j += 1;
 	}
-
 	/* copy temp to original interval */
 	for (i = start; i <= end; i += 1)
 	{
@@ -55,8 +50,6 @@ void merge(int *arr, int start, int mid, int end, int *temp)
 	}
 }
 
-/* arr is an array of integer type */
-/* start and end are the starting and ending index of current interval of arr */
 /**
  * mergeSort - merge sorts
  * @arr: the array
@@ -81,12 +74,13 @@ void mergeSort(int *arr, int start, int end, int *temp) {
  */
 void merge_sort(int *array, size_t size)
 {
-	int* temp;
-	if (size < 2) 
+	int *temp;
+
+	if (size < 2)
 	{
 		return;
 	}
-	temp = (int*)malloc((size) * sizeof(int));
-    mergeSort(array, 0, size - 1, temp);
+	temp = (int *)malloc((size) * sizeof(int));
+	mergeSort(array, 0, size - 1, temp);
 	free(temp);
 }
