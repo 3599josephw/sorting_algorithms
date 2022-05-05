0f09f882d82e5a4535c859ce210b61d44f9e269c
3599josephw <3599@holbertonschool.com>
2022-05-05 11:35:02 -0500

    updated with temp array in function defs

--- sort.h
+++ sort.h
@@ -25,6 +25,6 @@ void quickSort(int arr[], int low, int high, size_t size);
 int partition(int arr[], int low, int high, size_t size);
 void swap(int *a, int *b);
 void merge_sort(int *array, size_t size);
-void mergeSort(int *arr, int start, int end);
-void merge(int *Arr, int start, int mid, int end);
+void mergeSort(int *arr, int start, int end, int *temp);
+void merge(int *Arr, int start, int mid, int end, int *temp);
 #endif
